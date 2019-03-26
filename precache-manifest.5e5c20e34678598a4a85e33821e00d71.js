3e8615f951d2053384b65be5e38ec3d6fa06a5c2
xiaoyong <549469290@qq.com>
2019-03-26 22:32:59 +0800

    '1'

---
+++ precache-manifest.5e5c20e34678598a4a85e33821e00d71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba1f87ecbc18b2f0a8b6e29e8676ac82",
+    "url": "/static/media/logo.ba1f87ec.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "90cee7bddbca23221cb5",
+    "url": "/static/js/main.90cee7bd.chunk.js"
+  },
+  {
+    "revision": "44877eff7bcb275a0be6",
+    "url": "/static/js/1.44877eff.chunk.js"
+  },
+  {
+    "revision": "90cee7bddbca23221cb5",
+    "url": "/static/css/main.9170e6df.chunk.css"
+  },
+  {
+    "revision": "44877eff7bcb275a0be6",
+    "url": "/static/css/1.08b85205.chunk.css"
+  },
+  {
+    "revision": "d313ce11f8e60f666911e745a8fc1bdf",
+    "url": "/index.html"
+  }
+];
